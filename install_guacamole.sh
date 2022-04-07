4ee14878a9900c190888c1ff38ff92203aa246e0
Lyronn Levy <90407928+Fisstouk@users.noreply.github.com>
2022-04-08 01:16:24 +0200

    Install packages, build and install

--- install_guacamole.sh
+++ install_guacamole.sh
@@ -7,6 +7,7 @@
 # Date		: 03/04/2022
 # Changelog	: 03/04/2022-Creation du script, hash
 # Changelog	: 04/04/2022-Vérification gpg
+# Changelog	: 07/04/2022-Ajout paquets et installation
 
 # Affiche les commandes réalisées
 set -x
@@ -16,15 +17,18 @@ set -e
 
 function download_tools()
 {
-	apt install vim curl git gnupg -y
-
+	apt install make vim curl git gnupg -y
 }
 
-function download_check_hash()
+function download_guacamole_server()
 {
+
+	mkdir -vp /opt/guacamole/guacamole-server
+	cd /opt/guacamole/guacamole-server
+
 	# Téléchargement le tar gz de guacamole-server
 	# -O conserve le même nom que le fichier téléchargé
-	curl -O https://downloads.apache.org/guacamole/1.4.0/source/guacamole-server-1.4.0.tar.gz
+	curl -O https://downloads.apache.org/guacamole/1.4.0/source/guacamole-server-1.4.0.tar.gz 
 
 	# Téléchargement de la signature gpg
 	curl -O https://downloads.apache.org/guacamole/1.4.0/source/guacamole-server-1.4.0.tar.gz.asc
@@ -36,7 +40,7 @@ function download_check_hash()
 	curl -O https://downloads.apache.org/guacamole/KEYS
 
 	# Vérification du hash
-	sha256sum -c ~/guacamole-server-1.4.0.tar.gz.sha256
+	sha256sum -c /opt/guacamole/guacamole-server/guacamole-server-1.4.0.tar.gz.sha256
 
 	# Importation des clés gpp
 	gpg --import KEYS
@@ -53,6 +57,41 @@ function download_check_hash()
 	#	curl -O https://downloads.apache.org/guacamole/1.4.0/source/guacamole-server-1.4.0.tar.gz
 	#done
 
+}
+
+function required_dependencies()
+{
+	apt install libcairo2-dev \ 			# Utilisé par libguac pour le rendu graphique
+		libjpeg62-turbo-dev \			# Support JPEG 
+		libpng-dev \				# Ecrit des images PNG, le format principal de Guacamole
+		libtool-bin \ 				# Crée des bibliothèques compilées pour installer Guacamole
+		uuid-dev \ 				# Permet d'assigner des identifiants uniques aux utilisateurs et connexions
+		libossp-uuid-dev \ 
+		freerdp2-dev \				# Support de RDP
+		libpango1.0-dev \ 			# Support SSH, Kubernetes et telnet
+		libssh2-1-dev \ 			# Support SSH et SFTP
+		libvncserver-dev \			# Support VNC
+		libssl-dev -y				# Support SSL et TLS
+}
+
+function build_guacamole_server()
+{
+	# Extrait le code source de Guacamole	
+	tar xzf guacamole-server-1.4.0.tar.gz
+
+	cd guacamole-server-1.4.0
+
+	# Lancer configure pour déterminer les bibliothèques installées
+	./configure --with-init-dir=/etc/init.d
+
+	# Lancer make pour démarrer la compilation 
+	make
+
+	# Lancer l'installation
+	make install
+
+	# Mettre à jour le cache du système des bibliothèques installés
+	ldconfig
 }
 
 clear
